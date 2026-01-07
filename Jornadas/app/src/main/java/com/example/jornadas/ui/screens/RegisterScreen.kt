81001f4bc641e763b7c29e452d581ce4dc679592
WitaloFerreira <jwitalo085@gmail.com>
2026-01-07 01:27:24 -0300

    feat: Conecta a RegisterViewModel e adapta a UI

--- Jornadas/app/src/main/java/com/example/jornadas/ui/screens/RegisterScreen.kt
+++ Jornadas/app/src/main/java/com/example/jornadas/ui/screens/RegisterScreen.kt
@@ -8,15 +8,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Badge
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,16 +33,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jornadas.R
 import com.example.jornadas.ui.components.AppTextField
 import com.example.jornadas.ui.components.FilledButton
+import com.example.jornadas.viewmodels.register.RegisterViewModel
 
 @Composable
-fun RegisterScreen(onRegisterClick: () -> Unit, onLoginClick: () -> Unit, modifier: Modifier = Modifier) {
-    //mudar para o viewModel depois
-    var mock by remember { mutableStateOf("") } //mock para os textfields
+fun RegisterScreen(
+    onRegisterClick: () -> Unit,
+    onLoginClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: RegisterViewModel = viewModel()
+) {
+
+   val uiState by viewModel.uiState.collectAsState()
+
+    var passwordVisible by remember { mutableStateOf(false) }
+    var confirmPasswordVisible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(uiState.registerSuccess) {
+        if (uiState.registerSuccess) {
+            onRegisterClick()
+            viewModel.onRegisterConsumed()
+        }
+    }
+
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -74,45 +104,96 @@ fun RegisterScreen(onRegisterClick: () -> Unit, onLoginClick: () -> Unit, modifi
                 Spacer(modifier = Modifier.height(32.dp))
 
                 AppTextField(
-                    value = mock,
-                    onValueChange = { mock = it },
+                    value = uiState.user,
+                    onValueChange = { viewModel.onUserChange(it) },
                     label = "Usuário*",
+                    leadIcon = Icons.Default.Badge,
+                    errorMessage = uiState.userError,
                     modifier = Modifier
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 AppTextField(
-                    value = mock,
-                    onValueChange = { mock = it },
+                    value = uiState.email,
+                    onValueChange = { viewModel.onEmailChange(it) },
                     label = "Email*",
+                    leadIcon = Icons.Default.AccountCircle,
+                    errorMessage = uiState.emailError,
                     modifier = Modifier
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 AppTextField(
-                    value = mock,
-                    onValueChange = { mock = it },
+                    value = uiState.password,
+                    onValueChange = { viewModel.onPasswordChange(it) },
                     label = "Senha*",
-                    /*trailingIcon = Icons.Default.Visibility,*/
+                    leadIcon = Icons.Default.Lock,
+                    visualTransformation = if (passwordVisible) {
+                        VisualTransformation.None
+                    } else {
+                        PasswordVisualTransformation()
+                    },
+
+                    trailingIcon = {
+                        val image = if (passwordVisible) {
+                            Icons.Filled.Visibility
+                        } else {
+                            Icons.Filled.VisibilityOff
+                        }
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(
+                                imageVector = image,
+                                contentDescription = "Alternar visibilidade senha"
+                            )
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    errorMessage = uiState.passwordError,
                     modifier = Modifier
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 AppTextField(
-                    value = mock,
-                    onValueChange = { mock = it },
+                    value = uiState.confirmPassword,
+                    onValueChange = { viewModel.onConfirmPasswordChange(it) },
                     label = "Confirmar senha*",
-                    /*trailingIcon = Icons.Default.Visibility,*/
+                    leadIcon = Icons.Default.Lock,
+                    visualTransformation = if (confirmPasswordVisible) {
+                        VisualTransformation.None
+                    } else {
+                        PasswordVisualTransformation()
+                    },
+
+                    trailingIcon = {
+                        val image = if (confirmPasswordVisible) {
+                            Icons.Filled.Visibility
+                        } else {
+                            Icons.Filled.VisibilityOff
+                        }
+                        IconButton(onClick = { confirmPasswordVisible = !confirmPasswordVisible }) {
+                            Icon(
+                                imageVector = image,
+                                contentDescription = "Alternar visibilidade senha"
+                            )
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    imeAction = ImeAction.Done,
+                    errorMessage = uiState.confirmPasswordError,
                     modifier = Modifier
                 )
 
                 Spacer(modifier = Modifier.height(16.dp))
 
                 FilledButton(
-                    onclick = onRegisterClick,
+                    onclick = { viewModel.onRegister() },
                     stringResource(R.string.register),
                     modifier = Modifier
                         .fillMaxWidth()
