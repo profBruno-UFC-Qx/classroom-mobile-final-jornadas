7a600a6dadec6c1e20d43bc0ef35742924cc6f21
WitaloFerreira <jwitalo085@gmail.com>
2026-01-10 04:22:36 -0300

    feat: cria o card da memória

--- Jornadas/app/build.gradle.kts
+++ Jornadas/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
